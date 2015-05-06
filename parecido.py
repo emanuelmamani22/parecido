852d7e648cecba883b2e9e5a49193daee0664692
Emmanuel Mamani <emanuel_media15@hotmail.com>
2015-05-06 19:14:20 -0300

    correccion

--- parecido.py
+++ parecido.py
@@ -6,6 +6,7 @@ def letras (a,k):
         p2=0
         contl=0
         if k == f :
+            print 'hola'
             print "Hola"
         elif k != f :
             b=f
@@ -13,18 +14,18 @@ def letras (a,k):
             while cont < c :
                 if k[cont] == b[cont2] :
                     p1=p1+1
+                    if p1 >=3 :
+                        print "Quizas quiso decir "+b
+                        break
                 elif k[cont] != b[cont2] :
                     p2=p2+1
                 cont=cont+1
                 cont2=cont2+1
-                
-    if p1 >=3 :
-        print "Quizas quiso decir "+b
-    elif p2 >= 3 :
+    
+    if p2 >= 3 :
         print "Funcion letras" #borrar
         print "1. La palabra es correcta"
         print "2. Vuelva a escribir porfavor"
-        
         op=input('Ingrese la opcion deseada')
         if op == 1 :
             bb=[]
@@ -33,9 +34,8 @@ def letras (a,k):
         elif op == 2 :
             menu() #fin de la funcion
 
-
 def parecido (a): #funcion parecido
-    lista=['Miller','Urien','Canuelas']
+    lista=['Miller','Urien','Canuelas','hoola','saluda']
     contl=0
     a=a
     b=[]
